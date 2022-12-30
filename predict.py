72122f0b24532fa76e759ecab632d48e410513c4
arch <arch@local>
2022-12-30 17:10:41 +0100

    update config path

--- predict.py
+++ predict.py
@@ -25,7 +25,7 @@ class Predictor(BasePredictor):
         cfg = get_cfg()
         add_deeplab_config(cfg)
         add_maskformer2_config(cfg)
-        cfg.merge_from_file("Mask2Former/configs/coco/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_100ep.yaml")
+        cfg.merge_from_file("configs/coco/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_100ep.yaml")
         if not os.path.exists(weight_file):
             os.system("wget -c https://dl.fbaipublicfiles.com/maskformer/mask2former/coco/panoptic/maskformer2_swin_large_IN21k_384_bs16_100ep/model_final_f07440.pkl")
         cfg.MODEL.WEIGHTS = weight_file
